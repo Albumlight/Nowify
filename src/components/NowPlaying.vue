e12d405f5b356a90bf6deee86760837230d2e5dc
Project 43 <94372980+Albumlight@users.noreply.github.com>
2022-09-11 15:35:54 +0200

    Update NowPlaying.vue

--- src/components/NowPlaying.vue
+++ src/components/NowPlaying.vue
@@ -158,7 +158,7 @@ export default {
      */
     getEmptyPlayer() {
       return {
-        playing: false,
+        playing: true,
         trackAlbum: {},
         trackArtists: [],
         trackId: '',
