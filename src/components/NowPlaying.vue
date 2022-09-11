974f4b906cccfc0ac89b2fbce78526657cd0ad69
Project 43 <94372980+Albumlight@users.noreply.github.com>
2022-09-11 14:46:10 +0200

    Update NowPlaying.vue

--- src/components/NowPlaying.vue
+++ src/components/NowPlaying.vue
@@ -229,7 +229,7 @@ export default {
         trackArtists: this.playerResponse.item.artists.map(
           artist => artist.name
         ),
-        trackTitle: this.playerResponse.item.name,
+        trackTitle: this.playerResponse.item.album.name,
         trackId: this.playerResponse.item.id,
         trackAlbum: {
           title: this.playerResponse.item.album.name,
