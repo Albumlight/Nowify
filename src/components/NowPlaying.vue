8907bf0d3e9b13a04d75b2699587da3c69a46319
Project 43 <94372980+Albumlight@users.noreply.github.com>
2022-09-13 21:37:59 +0200

    Update NowPlaying.vue

--- src/components/NowPlaying.vue
+++ src/components/NowPlaying.vue
@@ -258,7 +258,7 @@ export default {
       this.swatches = albumColours
 
       this.colourPalette =
-        albumColours[((albumColours.length * 5)/6)]   // Set Swatchkleur klojo! 6 is licht, [((albumColours.length * 5)/6)] is donker
+        albumColours[((albumColours.length * 5)/6)]   // Set Swatchkleur: 5 donkerst, 2 ook goed, 3 en 4 licht, 
 
       this.$nextTick(() => {
         this.setAppColours()
