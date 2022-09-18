b55eec556d15412bcf9637ca5c19b290a212a1e9
Project 43 <94372980+Albumlight@users.noreply.github.com>
2022-09-18 12:17:32 +0200

    Update NowPlaying.vue

--- src/components/NowPlaying.vue
+++ src/components/NowPlaying.vue
@@ -7,7 +7,7 @@
     >
    <div class="now-playing__cover">
         <img
-          :src="player.artistId.image"
+          :src="artistId.image"
           :alt="player.trackTitle"
           class="now-playing__image"
         />
