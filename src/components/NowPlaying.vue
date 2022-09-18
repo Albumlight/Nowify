32e25ecc9cab9e19bd368ee12ce48187a689b62d
Project 43 <94372980+Albumlight@users.noreply.github.com>
2022-09-18 14:11:41 +0200

    Update NowPlaying.vue

--- src/components/NowPlaying.vue
+++ src/components/NowPlaying.vue
@@ -7,7 +7,7 @@
     >
    <div class="now-playing__cover">
         <img
-          :src="player.trackAlbum.imag"
+          :src="image:url"https://api.spotify.com/v1/artists/1GJ03RP8m15KzoFVsvRKSx"
           :alt="player.trackTitle"
           class="now-playing__image"
         />
