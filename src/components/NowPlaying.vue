5e3fdfab935f2f3469c31cf50c28b80044b77fc6
Project 43 <94372980+Albumlight@users.noreply.github.com>
2022-09-21 11:10:51 +0200

    Update NowPlaying.vue

--- src/components/NowPlaying.vue
+++ src/components/NowPlaying.vue
@@ -203,7 +203,7 @@ export default {
         trackId: this.playerResponse.item.id,
         trackAlbum: {
           title: this.playerResponse.item.album.name,
-          image: this.playerResponse.item.album.images[0].url
+          image: this.artist('62sPt3fswraiEPnKQpAbdE').images[0].url
         }
       }
     },
