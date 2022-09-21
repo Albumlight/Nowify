70ac9d4106ddf57dfb0637a311398a82e82cec04
Project 43 <94372980+Albumlight@users.noreply.github.com>
2022-09-21 11:26:45 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -63,7 +63,7 @@ export default {
      * @return {String}
      */
     getCurrentComponent() {
-      return this.auth.status === false ? 'Authorise' : 'NowPlaying'
+      return this.auth.status === false ? 'Authorise' : 'ArtistArt'
     }
   },
 
