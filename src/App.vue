1423af9c79baa48b81800c46b6d336f90f702f3c
Project 43 <94372980+Albumlight@users.noreply.github.com>
2022-09-22 20:18:16 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -63,7 +63,7 @@ export default {
      * @return {String}
      */
     getCurrentComponent() {
-      return this.auth.status === false ? 'Authorise' : 'artistImage' 
+      return this.auth.status === false ? 'Authorise' : 'ArtistImage' 
     }
   },
 
