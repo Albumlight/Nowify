aea6c41252fe0a663c7b91e69e96f1b579b3beec
Project 43 <94372980+Albumlight@users.noreply.github.com>
2022-09-23 19:39:14 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -44,7 +44,7 @@ export default {
         token: 'https://accounts.spotify.com/api/token',
         base: 'https://api.spotify.com/v1',
         nowPlaying: 'me/player/currently-playing',
-        ArtistArt: 'artists/1Talt4JuTAbZNKr6NAmq2I'
+        ArtistArt: 'artists/'
       },
       player: {
         playing: false,
