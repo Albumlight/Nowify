63445b0ee670d554dcd7b2059c9c512f5bad0020
Project 43 <94372980+Albumlight@users.noreply.github.com>
2022-09-23 17:07:50 +0200

    Update NowPlaying.vue

--- src/components/NowPlaying.vue
+++ src/components/NowPlaying.vue
@@ -120,7 +120,7 @@ export default {
         
         data = await response.json()
         this.artists = data
-        
+        console.log("Do what you want after completing the prev job.");
       } catch (error) {
         console.log(error)
       }
