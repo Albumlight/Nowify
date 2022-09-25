49462b497d846d4212bc0b292c9af129087ec049
Project 43 <94372980+Albumlight@users.noreply.github.com>
2022-09-25 20:06:26 +0200

    Update NowPlaying.vue

--- src/components/NowPlaying.vue
+++ src/components/NowPlaying.vue
@@ -109,7 +109,7 @@ const artistId = data.item.artists[0].id
 /**
  * Query the Spotify API and append the artist ID;
  */
-const artistResponse = await fetch(`${this.endpoints.base}/${this.endpoints.artist}/${artistId}`)
+const artistResponse = await fetch(`${this.endpoints.base}/${this.endpoints.artists}/${artistId}`)
 
 /**
  * Convert the API response to JSON
