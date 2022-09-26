6b7cae637cd3af37c37e4c5943b30beb394998e0
Project 43 <94372980+Albumlight@users.noreply.github.com>
2022-09-26 18:12:28 +0200

    Update NowPlaying.vue

--- src/components/NowPlaying.vue
+++ src/components/NowPlaying.vue
@@ -120,7 +120,7 @@ const artistResponse = await fetch(`${this.endpoints.base}/${this.endpoints.arti
  * Convert the API response to JSON
  */
 
-const { images[0]: artistData } = await artistResponse.json()
+const { images,[0]: artistData } = await artistResponse.json()
 this.artistData = artistData
 /**
  * Merge the player data and artist data to one object:
