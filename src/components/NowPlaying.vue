6c114f09fcdfc0d5f2eba0872f75e4f7c67df74a
Project 43 <94372980+Albumlight@users.noreply.github.com>
2022-10-01 17:51:18 +0200

    Update NowPlaying.vue

--- src/components/NowPlaying.vue
+++ src/components/NowPlaying.vue
@@ -19,7 +19,7 @@
         </div>
       <div class="now-playing__details">
         <h1 class="now-playing__track" v-text="player.trackTitle"></h1>
-        <h2 class="now-playing__artists" v-text="getTrackArtists"></h2>
+        <h2 class="now-playing__artists" v-text="player.trackAlbum.genre"></h2>
        </div>
        </div>
     <div v-else class="now-playing" :class="getNowPlayingClass()">
@@ -260,6 +260,7 @@ images: artistData.images[0].url,
           title: this.playerResponse.item.album.name,
           image: this.playerResponse.item.album.images[0].url,
           image2: this.playerResponse.artistData.images[0].url,
+          genre: this.playerResponse.artistData.genres[0],
           }
         }
     },
