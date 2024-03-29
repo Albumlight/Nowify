9a3b3b0eb16e7937dfb33621613326df955f1943
Project 43 <94372980+Albumlight@users.noreply.github.com>
2024-03-29 15:47:13 +0100

    Update NowPlaying.vue

--- src/components/NowPlaying.vue
+++ src/components/NowPlaying.vue
@@ -16,10 +16,7 @@
         <h1 class="now-playing__track" v-text="player.trackTitle"></h1>
         <h2 class="now-playing__artists" v-text="getTrackArtists"></h2>
       </div>
-      <button @click="show = !show">Toggle</button>
-<Transition>
-  <p v-if="show">hello</p>
-</Transition>
+      
     </div>
     <div v-else class="now-playing" :class="getNowPlayingClass()">
       <h1 class="now-playing__idle-heading">No music is playing 😔</h1>
